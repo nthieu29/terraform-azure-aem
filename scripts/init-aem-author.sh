923b528d34b1f32919979011e8c93ef5380637e1
nthieu29 <ngotrunghieu@live.com>
2021-09-04 21:00:18 +0800

    fix typo in scripts

--- scripts/init-aem-author.sh
+++ scripts/init-aem-author.sh
@@ -7,4 +7,4 @@ mkdir /home/azureuser/aem \
 && cd /home/azureuser/aem \
 && curl -o license.properties "https://aemdemostorage.blob.core.windows.net/aem65/setup/license.properties" \
 && curl -o cq5-author-p80.jar "https://aemdemostorage.blob.core.windows.net/aem65/setup/aem-author-4502.jar" \
-&& sudo java -jar cq-author-p80.jar -nointeractive
+&& sudo java -jar cq5-author-p80.jar -nointeractive
